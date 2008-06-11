aa029f26e33fdbc9bb2b75b5fcc1713e39de2b0b
lorenzo <sha1-63d4ab710c73f9120caf0cd77fadeec8376dd4f1@cern.ch>
2008-06-11 12:01:17 +0000

    *** empty log message ***

--- plugins/SealModule.cc
+++ plugins/SealModule.cc
@@ -1,10 +1,9 @@
-// $Id: SealModule.cc,v 1.7 2008/05/30 09:25:48 bachtis Exp $
+// $Id: SealModule.cc,v 1.5.2.1 2008/06/11 11:54:51 lorenzo Exp $
 
 #include "DQM/HLTEvF/interface/HltAnalyzer.h"
 #include "DQM/HLTEvF/interface/PathTimerService.h"
 #include "DQM/HLTEvF/interface/PathTimerInserter.h"
 #include "DQM/HLTEvF/interface/HLTMuonDQMSource.h"
-#include "DQM/HLTEvF/interface/HLTTauDQMSource.h"
 #include "DQM/HLTEvF/interface/HLTMonElectron.h"
 #include "DQM/HLTEvF/interface/FourVectorHLT.h"
 #include "DQM/HLTEvF/interface/HLTEventInfoClient.h"
@@ -19,7 +18,6 @@ DEFINE_FWK_SERVICE(PathTimerService);
 DEFINE_FWK_MODULE(PathTimerInserter);
 DEFINE_FWK_MODULE(HltAnalyzer);
 DEFINE_FWK_MODULE(HLTMuonDQMSource);
-DEFINE_FWK_MODULE(HLTTauDQMSource);
 DEFINE_FWK_MODULE(HLTMonElectron);
 DEFINE_FWK_MODULE(FourVectorHLT);
 DEFINE_FWK_MODULE(HLTEventInfoClient);
