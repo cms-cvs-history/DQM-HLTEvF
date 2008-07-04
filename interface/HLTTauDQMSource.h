6ef028760b53040351ef57215bf4d3de83c4bbfd
cmsbuild <>
2008-07-04 10:13:09 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- HLTEvF-V03-00-23'.
    Sprout from master 2008-06-30 15:30:04 UTC bjbloom <sha1-b16c2c250931f43e06dcda266c05bdc208573998@cern.ch> 'Uses HLTMon for Muons'
    Cherrypick from master 2008-07-04 10:13:08 UTC Michail Bachtis <sha1-eaf6ea55826e5289c86058cfc492a286c076afdf@cern.ch> '*** empty log message ***':
        data/HLTMonTau.cfi
        interface/HLTTauDQMSource.h
        plugins/HLTTauDQMSource.cc
    Cherrypick from master 2008-03-05 09:54:04 UTC Muriel Vander Donckt <sha1-7bc4d0901fcc12bb4f6f4a6f8b2f4b01fc0bc1a4@cern.ch> 'Iso patch and merging with 18X':
        plugins/HLTMuonDQMSource.cc

--- interface/HLTTauDQMSource.h
+++ interface/HLTTauDQMSource.h
@@ -6,6 +6,7 @@ Author : Michail Bachtis
 University of Wisconsin-Madison
 bachtis@hep.wisc.edu
 */
+
  
 #include <memory>
 #include <unistd.h>
@@ -27,9 +28,7 @@ bachtis@hep.wisc.edu
 #include "DataFormats/HLTReco/interface/TriggerEvent.h"
 
 
-
 //Electron includes
-
 #include "DataFormats/EgammaCandidates/interface/Electron.h"
 #include "DataFormats/EgammaCandidates/interface/ElectronFwd.h"
 
@@ -88,11 +87,9 @@ protected:
 
   void beginLuminosityBlock(const edm::LuminosityBlock& lumiSeg, 
                             const edm::EventSetup& context) ;
-
   /// DQM Client Diagnostic
   void endLuminosityBlock(const edm::LuminosityBlock& lumiSeg, 
                           const edm::EventSetup& c);
-
   /// EndRun
   void endRun(const edm::Run& r, const edm::EventSetup& c);
 
@@ -114,6 +111,8 @@ private:
   int counterEvt_;      ///counter
   int prescaleEvt_;     ///every n events 
   bool disable_;        ///disable
+
+
   bool verbose_;          
   unsigned  nTriggeredTaus_;
   double EtMin_;
@@ -124,14 +123,8 @@ private:
 
   //get The Jet Collections per filter level
   edm::InputTag triggerEvent_;
-
-  //L1 Specific stuff
-  // edm::InputTag L1Taus_;
-  //edm::InputTag L1GTReadoutRecord_;
-  //edm::InputTag L1GTObjectMap_;
- 
-
   edm::InputTag l1Filter_;
+  edm::InputTag l2Reco_;
   edm::InputTag l2Filter_;
   edm::InputTag l25Filter_;
   edm::InputTag l3Filter_;
@@ -140,10 +133,13 @@ private:
   edm::InputTag l2BackupPath_;
   edm::InputTag l25BackupPath_;
   edm::InputTag l3BackupPath_;
-
+  std::vector<int> prescales_;
 
   //Correlations with other Triggers
   std::vector<edm::InputTag> refFilters_;
+  std::vector<int> ref_prescales_;
+
+
   //  std::vector<int> refIDs_;
   std::vector<double> PtCut_;
   std::vector<std::string> refFilterDesc_;
@@ -157,13 +153,20 @@ private:
   //L25 Monitoring Parameters
   bool doL25Monitoring_;
   edm::InputTag l25IsolInfo_;
+  //  double matchCone_;
+  //double minTrackPt_;
+
 
   //L3 Monitoring Parameters
   bool doL3Monitoring_;
   edm::InputTag l3IsolInfo_;
+  // double l3matchCone_;
+  //double l3minTrackPt_;
+
 
   //Number of Tau Events passed the triggers
   int NEventsPassedL1;
+  int NEventsPassedL2Reco;
   int NEventsPassedL2;
   int NEventsPassedL25;
   int NEventsPassedL3;
@@ -178,6 +181,7 @@ private:
 
   //Number of Tau Events passed the triggers matched to reference objects
   std::vector<int> NEventsPassedRefL1;
+  std::vector<int> NEventsPassedRefL2Reco;
   std::vector<int> NEventsPassedRefL2;
   std::vector<int> NEventsPassedRefL25;
   std::vector<int> NEventsPassedRefL3;
@@ -261,7 +265,6 @@ private:
   void doL25(const edm::Event& e, const edm::EventSetup& c);
   void doL3(const edm::Event& e, const edm::EventSetup& c);
 
-
   bool match(const LV&,const LVColl& /*trigger::VRelectron&*/,double,double);
   std::vector<double> calcEfficiency(int,int);
   LVColl importObjectColl(edm::InputTag&,int,const edm::Event&);
@@ -269,7 +272,6 @@ private:
   
   //Basic Histogram formating since we have not put any render plugins yet
   void formatHistogram(MonitorElement*,int);
-
 
 };
 
