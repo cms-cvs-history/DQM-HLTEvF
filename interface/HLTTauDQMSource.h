9642946a0f1baaf95fd98ba052add8e1447fb7b6
cmsbuild <>
2008-07-08 17:22:41 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- HLTEvF-V03-00-28'.
    Sprout from master 2008-06-30 15:30:04 UTC bjbloom <sha1-b16c2c250931f43e06dcda266c05bdc208573998@cern.ch> 'Uses HLTMon for Muons'
    Cherrypick from master 2008-07-08 17:22:40 UTC Michail Bachtis <sha1-eaf6ea55826e5289c86058cfc492a286c076afdf@cern.ch> 'Added backups for MuTau in TauDQM':
        data/HLTMonTau.cfi
        interface/HLTTauDQMSource.h
        plugins/HLTTauDQMSource.cc
    Cherrypick from master 2008-03-05 09:54:04 UTC Muriel Vander Donckt <sha1-7bc4d0901fcc12bb4f6f4a6f8b2f4b01fc0bc1a4@cern.ch> 'Iso patch and merging with 18X':
        plugins/HLTMuonDQMSource.cc

--- interface/HLTTauDQMSource.h
+++ interface/HLTTauDQMSource.h
@@ -6,6 +6,7 @@ Author : Michail Bachtis
 University of Wisconsin-Madison
 bachtis@hep.wisc.edu
 */
+
  
 #include <memory>
 #include <unistd.h>
@@ -26,10 +27,11 @@ bachtis@hep.wisc.edu
 #include "Math/GenVector/VectorUtil.h"
 #include "DataFormats/HLTReco/interface/TriggerEvent.h"
 
-
+//MET Includes
+#include "DataFormats/METReco/interface/CaloMETCollection.h"
+#include "DataFormats/METReco/interface/CaloMET.h"
 
 //Electron includes
-
 #include "DataFormats/EgammaCandidates/interface/Electron.h"
 #include "DataFormats/EgammaCandidates/interface/ElectronFwd.h"
 
@@ -37,11 +39,9 @@ bachtis@hep.wisc.edu
 #include "DataFormats/RecoCandidate/interface/RecoChargedCandidateFwd.h"
 #include "DataFormats/RecoCandidate/interface/RecoChargedCandidate.h"
 
-
 //Photon Includes
 #include "DataFormats/RecoCandidate/interface/RecoEcalCandidateFwd.h"
 #include "DataFormats/RecoCandidate/interface/RecoEcalCandidate.h"
-
 
 //Track Include
 #include "DataFormats/HcalIsolatedTrack/interface/IsolatedPixelTrackCandidateFwd.h"
@@ -56,12 +56,6 @@ bachtis@hep.wisc.edu
 #include "DataFormats/TrackReco/interface/Track.h"
 
 
-// L1 Trigger data formats
-#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutRecord.h"
-#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerReadoutSetupFwd.h"
-#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerObjectMapRecord.h"
-#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerObjectMapFwd.h"
-#include "DataFormats/L1GlobalTrigger/interface/L1GlobalTriggerObjectMap.h"
 
 //
 // class declaration
@@ -86,13 +80,12 @@ protected:
   /// Fake Analyze
   void analyze(const edm::Event& e, const edm::EventSetup& c) ;
 
+  ///Luminosity Block 
   void beginLuminosityBlock(const edm::LuminosityBlock& lumiSeg, 
                             const edm::EventSetup& context) ;
-
   /// DQM Client Diagnostic
   void endLuminosityBlock(const edm::LuminosityBlock& lumiSeg, 
                           const edm::EventSetup& c);
-
   /// EndRun
   void endRun(const edm::Run& r, const edm::EventSetup& c);
 
@@ -114,6 +107,8 @@ private:
   int counterEvt_;      ///counter
   int prescaleEvt_;     ///every n events 
   bool disable_;        ///disable
+
+
   bool verbose_;          
   unsigned  nTriggeredTaus_;
   double EtMin_;
@@ -124,14 +119,8 @@ private:
 
   //get The Jet Collections per filter level
   edm::InputTag triggerEvent_;
-
-  //L1 Specific stuff
-  // edm::InputTag L1Taus_;
-  //edm::InputTag L1GTReadoutRecord_;
-  //edm::InputTag L1GTObjectMap_;
- 
-
   edm::InputTag l1Filter_;
+  edm::InputTag l2Reco_;
   edm::InputTag l2Filter_;
   edm::InputTag l25Filter_;
   edm::InputTag l3Filter_;
@@ -144,10 +133,17 @@ private:
 
   //Correlations with other Triggers
   std::vector<edm::InputTag> refFilters_;
+  std::vector<int> refIDs_;
+
   //  std::vector<int> refIDs_;
   std::vector<double> PtCut_;
   std::vector<std::string> refFilterDesc_;
   double corrDeltaR_;
+
+  //Missing Et
+   std::vector<double> METCut_;
+   edm::InputTag MET_;
+
 
   //L2 Monitoring Parameters
   bool doL2Monitoring_;
@@ -158,32 +154,52 @@ private:
   bool doL25Monitoring_;
   edm::InputTag l25IsolInfo_;
 
+
   //L3 Monitoring Parameters
   bool doL3Monitoring_;
   edm::InputTag l3IsolInfo_;
 
+
   //Number of Tau Events passed the triggers
   int NEventsPassedL1;
+  int NEventsPassedL2Reco;
   int NEventsPassedL2;
   int NEventsPassedL25;
   int NEventsPassedL3;
 
-  //Number of Tau Events passed the Backup Triggers
-  int NEventsPassedMainFilter;
-  int NEventsPassedL1Backup;
-  int NEventsPassedL2Backup;
-  int NEventsPassedL25Backup;
-  int NEventsPassedL3Backup;
+  //Efficiency definitions for the backup triggers
+  int L1EffNum;
+  int L1EffDenom;
+  int L2EffNum;
+  int L2EffDenom;
+  int L25EffNum;
+  int L25EffDenom;
+  int L3EffNum;
+  int L3EffDenom;
 
 
   //Number of Tau Events passed the triggers matched to reference objects
   std::vector<int> NEventsPassedRefL1;
+  std::vector<int> NEventsPassedRefL2Reco;
   std::vector<int> NEventsPassedRefL2;
   std::vector<int> NEventsPassedRefL25;
   std::vector<int> NEventsPassedRefL3;
 
-  //Number of reference objects
-  std::vector<int> NRefEvents;
+
+  //Efficiency measurements for the reference triggers
+  std::vector<int> L1EffNumRef;
+  std::vector<int> L2RecoEffNumRef;
+  std::vector<int> L2EffNumRef;
+  std::vector<int> L25EffNumRef;
+  std::vector<int> L3EffNumRef;
+
+  std::vector<int> DenomRef;
+
+  //std::vector<int> L2RecoEffDenomRef;
+  //std::vector<int> L2EffDenomRef;
+  //std::vector<int> L25EffDenomRef;
+  //std::vector<int> L3EffDenomRef;
+
 
   //MonitorElements(Trigger Bits and Efficiency with ref to L1)
   MonitorElement *triggerBitInfo_;
@@ -261,14 +277,10 @@ private:
   void doL25(const edm::Event& e, const edm::EventSetup& c);
   void doL3(const edm::Event& e, const edm::EventSetup& c);
 
-
   bool match(const LV&,const LVColl& /*trigger::VRelectron&*/,double,double);
   std::vector<double> calcEfficiency(int,int);
   LVColl importObjectColl(edm::InputTag&,int,const edm::Event&);
-  LVColl importFilterColl(edm::InputTag&,const edm::Event&);
-  
-  //Basic Histogram formating since we have not put any render plugins yet
-  void formatHistogram(MonitorElement*,int);
+  LVColl importFilterColl(edm::InputTag&,int,const edm::Event&);
 
 
 };
