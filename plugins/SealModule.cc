2efb817e383b233d2686acaded7116c32d7defb7
Peter Wittich <sha1-1c129fc6f1e49c3fc8fb03accebf4e9c0677cc9f@cern.ch>
2008-08-13 15:30:38 +0000

    remove HLT Analzer, version for 2.0.x

--- plugins/SealModule.cc
+++ plugins/SealModule.cc
@@ -1,6 +1,5 @@
-// $Id: SealModule.cc,v 1.5.2.3 2008/06/25 14:31:44 bachtis Exp $
+// $Id: SealModule.cc,v 1.5.2.4 2008/06/30 14:20:46 bjbloom Exp $
 
-#include "DQM/HLTEvF/interface/HltAnalyzer.h"
 #include "DQM/HLTEvF/interface/PathTimerService.h"
 #include "DQM/HLTEvF/interface/PathTimerInserter.h"
 #include "DQM/HLTEvF/interface/HLTMuonDQMSource.h"
@@ -18,7 +17,6 @@ DEFINE_FWK_SERVICE(PathTimerService);
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(PathTimerInserter);
-DEFINE_FWK_MODULE(HltAnalyzer);
 DEFINE_FWK_MODULE(HLTMuonDQMSource);
 DEFINE_FWK_MODULE(HLTTauDQMSource);
 DEFINE_FWK_MODULE(HLTMonElectron);
