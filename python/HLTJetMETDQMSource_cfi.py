3eab4f6b29d5c3a831caf485636b2c653cf67fc3
Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch>
2010-05-02 08:07:49 +0000

    fix python module type

--- python/HLTJetMETDQMSource_cfi.py
+++ python/HLTJetMETDQMSource_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-hltResults = cms.EDFilter("HLTJetMETDQMSource",
+hltResults = cms.EDAnalyzer("HLTJetMETDQMSource",
     plotAll = cms.untracked.bool(False),
     ptMax = cms.untracked.double(100.0),
     ptMin = cms.untracked.double(0.0),
